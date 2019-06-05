b7703050cc69c1129335d3c83307bf3627241b6c
Monjoie11 <jeff.franken@gmail.com>
2019-06-05 15:05:57 -0600

    added clock display and some new methods for initGame

--- app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/MainActivity.java
@@ -42,11 +42,13 @@ public class MainActivity extends AppCompatActivity
   private boolean running;
   private boolean complete;
   private TextView valueDisplay;
+  private TextView clockDisplay;
   private ViewGroup valueContainer;
   private Rect displayRect = new Rect();
   private GestureDetectorCompat detector;
   private Timer valueTimer;
-  private  Timer gameTimer;
+  private Timer gameTimer;
+  private Timer clockTimer;
   private SharedPreferences preferences;
   private Game game;
   private int numDigits;
@@ -54,9 +56,9 @@ public class MainActivity extends AppCompatActivity
   private int gameDuration;
   private long gameTimerStart;
   private long gameTimeElapsed;
-  String gameDataKey;
-  String gameTimeElapsedKey;
-
+  private String gameDataKey;
+  private String gameTimeElapsedKey;
+  private String clockFormat;
 
   /**
    * Initializes this activity when created, and when restored after {@link #onDestroy()} (for
@@ -70,6 +72,7 @@ public class MainActivity extends AppCompatActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     valueDisplay = findViewById(R.id.value_display);
+    clockDisplay = findViewById(R.id.clock_display);
     valueContainer = (ViewGroup) valueDisplay.getParent();
     detector = new GestureDetectorCompat(this, new FlingListener());
     valueContainer.setOnTouchListener(this);
@@ -77,33 +80,36 @@ public class MainActivity extends AppCompatActivity
     preferences.registerOnSharedPreferenceChangeListener(this);
     readSettings();
     gameDataKey = getString(R.string.game_data_key);
-    gameTimeElapsedKey = getString((R.string.game_time_elapsed_key));
+    gameTimeElapsedKey = getString(R.string.game_time_elapsed_key);
+    clockFormat = getString(R.string.clock_format);
     if (savedInstanceState != null) {
       game = (Game) savedInstanceState.getSerializable(gameDataKey);
       gameTimeElapsed = savedInstanceState.getLong(gameTimeElapsedKey, 0);
+      gameTimerStart = System.currentTimeMillis();
+      updateClock();
     }
     if (game == null) {
-      game = new Game(timeLimit, numDigits, gameDuration);
+      initGame();
     }
   }
 
   /**
-   * Updates valueTimer(s) and UI to return display &amp; game to the pre-{@link #onPause()} state.
+   * Updates timer(s) and UI to return display &amp; game to the pre-{@link #onPause()} state.
    */
   @Override
   protected void onResume() {
     super.onResume();
-    // TODO Resume game if running.
+    // TODO Resume game if running?
   }
 
   /**
-   * Captures current state of valueTimer(s) to fields, for possible saving by subsequent {@link
+   * Captures current state of timer(s) to fields, for possible saving by subsequent {@link
    * #onSaveInstanceState(Bundle)} invocation and/or restoration by {@link #onResume()} invocation.
    */
   @Override
   protected void onPause() {
     super.onPause();
-   pauseGame();
+    pauseGame();
   }
 
   /**
@@ -114,7 +120,6 @@ public class MainActivity extends AppCompatActivity
   @Override
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
-    String gameDataKey = getString(R.string.game_data_key);
     outState.putSerializable(gameDataKey, game);
     outState.putLong(gameTimeElapsedKey, gameTimeElapsed);
   }
@@ -163,13 +168,10 @@ public class MainActivity extends AppCompatActivity
     Intent intent;
     switch (item.getItemId()) {
       case R.id.reset:
-        game = new Game(timeLimit, numDigits, gameDuration);
-        gameTimeElapsed = 0;
-        complete = false;
         Toast.makeText(this, R.string.reset_message, Toast.LENGTH_LONG).show();
+        initGame();
         pauseGame();
         break;
-        //TODO Combine invocations of Game constructor
       case R.id.play:
         resumeGame();
         break;
@@ -230,7 +232,7 @@ public class MainActivity extends AppCompatActivity
   public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
     readSettings();
     pauseGame();
-    game = new Game(timeLimit, numDigits, gameDuration);
+    initGame();
   }
 
   private void readSettings() {
@@ -243,40 +245,49 @@ public class MainActivity extends AppCompatActivity
         res.getInteger(R.integer.game_time_default));
   }
 
+  private void initGame() {
+    game = new Game(timeLimit, numDigits, gameDuration);
+    gameTimeElapsed = 0;
+    complete = false;
+    gameTimerStart = System.currentTimeMillis();
+    updateClock();
+  }
+
   private void pauseGame() {
     running = false;
-   stopValueTimer();
-   stopGameTimer();
-   valueDisplay.setText("");
-    // TODO Update any additional necessary fields.
+    stopValueTimer();
+    stopGameTimer();
+    valueDisplay.setText("");
     invalidateOptionsMenu();
   }
 
   private void resumeGame() {
     running = true;
     if (game == null) {
-      game = new Game(timeLimit, numDigits, gameDuration);
-      gameTimeElapsed = 0;
+      initGame();
     }
     updateValue();
     startGameTimer();
     startValueTimer();
-    // TODO Update any additional necessary fields.
     invalidateOptionsMenu();
   }
 
-  private void stopValueTimer(){
-    if (valueTimer != null){
+  private void stopValueTimer() {
+    if (valueTimer != null) {
       valueTimer.cancel();
       valueTimer = null;
     }
   }
 
-  private void stopGameTimer (){
-    if (gameTimer != null){
+  private void stopGameTimer() {
+    if (gameTimer != null) {
       gameTimer.cancel();
       gameTimer = null;
       gameTimeElapsed += System.currentTimeMillis() - gameTimerStart;
+    }
+    if (clockTimer != null) {
+      clockTimer.cancel();
+      clockTimer = null;
     }
   }
 
@@ -286,10 +297,10 @@ public class MainActivity extends AppCompatActivity
     game.add(round);
     ImageView indicator;
     Animator fade = AnimatorInflater.loadAnimator(this, R.animator.indicator_fade);
-    switch (category){
+    switch (category) {
       case FIZZ:
         indicator = findViewById(round.isCorrect() ?
-           R.id.correct_fizz_indicator : R.id.incorrect_fizz_indicator);
+            R.id.correct_fizz_indicator : R.id.incorrect_fizz_indicator);
         break;
       case BUZZ:
         indicator = findViewById(round.isCorrect() ?
@@ -302,11 +313,11 @@ public class MainActivity extends AppCompatActivity
       default:
         indicator = findViewById(round.isCorrect() ?
             R.id.correct_neither_indicator : R.id.incorrect_neither_indicator);
+        break;
     }
     fade.setTarget(indicator);
     fade.start();
   }
-
 
   private void updateValue() {
     int valueLimit = (int) Math.pow(10, numDigits) - 1;
@@ -338,9 +349,27 @@ public class MainActivity extends AppCompatActivity
   }
 
   private void startGameTimer() {
-      gameTimer = new Timer();
-      gameTimer.schedule(new GameTimeoutTask(), gameDuration * 1000 - gameTimeElapsed);
-      gameTimerStart = System.currentTimeMillis();
+    gameTimer = new Timer();
+    gameTimer.schedule(new GameTimeoutTask(), 1000L * gameDuration - gameTimeElapsed);
+    gameTimerStart = System.currentTimeMillis();
+    clockTimer = new Timer();
+    clockTimer.schedule(new ClockTimerTask(), 0, 100);
+  }
+
+  private void updateClock() {
+    long remaining = (running  || gameTimeElapsed > 0) ?
+        gameDuration * 1000L - (System.currentTimeMillis() - gameTimerStart + gameTimeElapsed) :
+        gameDuration * 1000L;
+    int minutes;
+    double seconds;
+    if (remaining > 0) {
+      minutes = (int) (remaining / 60_000);
+      seconds = (remaining % 60_000) / 1000.0;
+    } else {
+      minutes = 0;
+      seconds = 0;
+    }
+    clockDisplay.setText(String.format(clockFormat, minutes, seconds));
   }
 
   private class TimeoutTask extends TimerTask {
@@ -355,20 +384,29 @@ public class MainActivity extends AppCompatActivity
     }
 
   }
-  private class GameTimeoutTask extends TimerTask{
+
+  private class GameTimeoutTask extends TimerTask {
 
     @Override
     public void run() {
       complete = true;
       runOnUiThread(() -> {
         pauseGame();
-        Toast.makeText(MainActivity.this, "Where you at, playa? Game resetting", Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, R.string.time_expired_message, Toast.LENGTH_LONG).show();
         showStats();
       });
     }
 
   }
 
+  private class ClockTimerTask extends TimerTask {
+
+    @Override
+    public void run() {
+      runOnUiThread(() -> updateClock());
+    }
+
+  }
 
   private class FlingListener extends GestureDetector.SimpleOnGestureListener {
 
@@ -377,46 +415,53 @@ public class MainActivity extends AppCompatActivity
 
     private float originX;
     private float originY;
+    private int dragValue;
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-      valueDisplay.setTranslationX(e2.getX() - originX);
-      valueDisplay.setTranslationY(e2.getY() - originY);
-      return true;
+      boolean handled = false;
+      if (value == dragValue) {
+        valueDisplay.setTranslationX(e2.getX() - originX);
+        valueDisplay.setTranslationY(e2.getY() - originY);
+        handled = true;
+      }
+      return handled;
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
       boolean handled = false;
-      int containerHeight = valueContainer.getHeight();
-      int containerWidth = valueContainer.getWidth();
-      int radiusX = containerWidth / RADIUS_FACTOR;
-      int radiusY = containerHeight / RADIUS_FACTOR;
-      double deltaX = e2.getX() - e1.getX();
-      double deltaY = e2.getY() - e1.getY();
-      double ellipticalDistance =
-          deltaX * deltaX / radiusX / radiusX + deltaY * deltaY / radiusY / radiusY;
-      double speed = Math.hypot(velocityX, velocityY);
-      if (speed >= SPEED_THRESHOLD && ellipticalDistance >= 1) {
-        stopValueTimer();
-        Category selection;
-        if (Math.abs(deltaY) * containerWidth <= Math.abs(deltaX) * containerHeight) {
-          if (deltaX > 0) {
-            selection = Category.BUZZ;
+      if (value == dragValue) {
+        int containerHeight = valueContainer.getHeight();
+        int containerWidth = valueContainer.getWidth();
+        int radiusX = containerWidth / RADIUS_FACTOR;
+        int radiusY = containerHeight / RADIUS_FACTOR;
+        double deltaX = e2.getX() - e1.getX();
+        double deltaY = e2.getY() - e1.getY();
+        double ellipticalDistance =
+            deltaX * deltaX / radiusX / radiusX + deltaY * deltaY / radiusY / radiusY;
+        double speed = Math.hypot(velocityX, velocityY);
+        if (speed >= SPEED_THRESHOLD && ellipticalDistance >= 1) {
+          stopValueTimer();
+          Category selection;
+          if (Math.abs(deltaY) * containerWidth <= Math.abs(deltaX) * containerHeight) {
+            if (deltaX > 0) {
+              selection = Category.BUZZ;
+            } else {
+              selection = Category.FIZZ;
+            }
           } else {
-            selection = Category.FIZZ;
+            if (deltaY > 0) {
+              selection = Category.NEITHER;
+            } else {
+              selection = Category.FIZZ_BUZZ;
+            }
           }
-        } else {
-          if (deltaY > 0) {
-            selection = Category.NEITHER;
-          } else {
-            selection = Category.FIZZ_BUZZ;
-          }
+          recordRound(selection);
+          updateValue();
+          startValueTimer();
+          handled = true;
         }
-        recordRound(selection);
-        updateValue();
-        startValueTimer();
-        handled = true;
       }
       return handled;
     }
@@ -427,6 +472,7 @@ public class MainActivity extends AppCompatActivity
       if (displayRect.contains(Math.round(evt.getX()), Math.round(evt.getY()))) {
         originX = evt.getX() - valueDisplay.getTranslationX();
         originY = evt.getY() - valueDisplay.getTranslationY();
+        dragValue = value;
         handled = true;
       }
       return handled;
@@ -435,4 +481,3 @@ public class MainActivity extends AppCompatActivity
   }
 
 }
-
