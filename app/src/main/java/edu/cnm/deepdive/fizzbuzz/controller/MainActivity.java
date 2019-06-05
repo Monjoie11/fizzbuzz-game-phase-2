b232f0661a38ec0965d68847c47b3ef9beb2e35c
Monjoie11 <jeff.franken@gmail.com>
2019-06-05 09:08:28 -0600

    added direction markers

--- app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzz/controller/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 import androidx.preference.PreferenceManager;
@@ -165,6 +166,7 @@ public class MainActivity extends AppCompatActivity
         game = new Game(timeLimit, numDigits, gameDuration);
         gameTimeElapsed = 0;
         complete = false;
+        Toast.makeText(this, R.string.reset_message, Toast.LENGTH_LONG).show();
         pauseGame();
         break;
         //TODO Combine invocations of Game constructor
@@ -179,15 +181,19 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
         break;
       case R.id.status:
-        intent = new Intent(this, StatusActivity.class);
-        intent.putExtra(getString(R.string.game_data_key), game);
-        startActivity(intent);
+        showStats();
         break;
       default:
         handled = super.onOptionsItemSelected(item);
         break;
     }
     return handled;
+  }
+
+  private void showStats() {
+    Intent intent = new Intent(this, StatusActivity.class);
+    intent.putExtra(getString(R.string.game_data_key), game);
+    startActivity(intent);
   }
 
   /**
@@ -354,7 +360,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void run() {
       complete = true;
-      runOnUiThread(() -> pauseGame());
+      runOnUiThread(() -> {
+        pauseGame();
+        Toast.makeText(MainActivity.this, "Where you at, playa? Game resetting", Toast.LENGTH_LONG).show();
+        showStats();
+      });
     }
 
   }
